2a99f1962bfd1884a5335163b15fb7c9bcbc03ec
Ekaterina Nechkina <e.nechkina@yandex.ru>
2023-07-22 23:10:01 +0300

    add sameSite none

--- controllers/users.js
+++ controllers/users.js
@@ -15,6 +15,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         httpOnly: true,
         secure: NODE_ENV === 'production',
+        sameSite: 'none',
       })
         .send({ message: 'Вы авторизовались' });
     })
@@ -22,7 +23,14 @@ module.exports.login = (req, res, next) => {
 };
 
 module.exports.signoutUser = (req, res) => {
-  res.clearCookie('jwt').send({ message: 'Кука удалена' });
+  const { NODE_ENV } = process.env;
+
+  res.clearCookie('jwt', {
+    httpOnly: true,
+    secure: NODE_ENV === 'production',
+    sameSite: 'none',
+  })
+    .send({ message: 'Кука удалена' });
 };
 
 module.exports.createUser = (req, res, next) => {
